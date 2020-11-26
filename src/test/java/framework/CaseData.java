01cdd2a8dac3be32d0e32383681fe93ed143142e
wuchao <n2s_LZ39>
2020-11-26 22:56:23 +0800

    commit CaseData

--- src/test/java/framework/CaseData.java
+++ src/test/java/framework/CaseData.java
@@ -2,6 +2,7 @@ package framework;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ public class CaseData {
     public List<String> data;
     public List<HashMap<String, Object>> steps;
     private WebDriver driver;
+    private WebElement element;
 
     public void run(){
         steps.forEach(step -> {
@@ -21,10 +23,30 @@ public class CaseData {
             }else if (step.keySet().contains("get")){
                 driver.get(step.get("get").toString());
             } else if (step.keySet().contains("implicitlyWait")){
+                System.out.println("延迟等待时间：" + step.get("implicitlyWait").toString());
                 driver.manage().timeouts().implicitlyWait(Integer.parseInt(step.get("implicitlyWait").toString()),TimeUnit.SECONDS);
             }else if (step.keySet().contains("find")){
-                ArrayList<By> arrayList = new ArrayList();
+                ArrayList<By> arrayList = new ArrayList<>();
+                ((HashMap<String,String>)step.get("find")).entrySet().forEach(str ->{
+                    if (str.getKey().contains("id")){
+                        System.out.println("元素:" + str.getValue());
+                        arrayList.add(By.id(str.getValue()));
+                    }
+                    if (str.getKey().contains("xpath")){
+                        arrayList.add(By.xpath(str.getValue()));
+                    }
+                    if (str.getKey().contains("cssSelector")){
+                        arrayList.add(By.cssSelector(str.getValue()));
+                    }
+                    element = driver.findElement(arrayList.get(0));
+                });
+            }
 
+            if (step.keySet().contains("click")){
+                element.click();
+            }
+            if (step.keySet().contains("sendkeys")){
+                element.sendKeys("search demo");
             }
         });
     }
