f20d6aa808810aa817adfef2e2f0fdc97f5d30c9
wuchao <n2s_LZ39>
2020-12-01 23:41:08 +0800

    update PO yaml

--- src/test/java/framework/SeleniumTest.java
+++ src/test/java/framework/SeleniumTest.java
@@ -1,27 +1,62 @@
 package framework;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumTest extends TestCase{
+    private ChromeDriver driver;
+    private WebElement currentElement;
 
     public void run(){
         steps.forEach(step -> {
-            String key = step.keySet().iterator().next();
-            if (key.contains("init")){
-                ArrayList<String> value = (ArrayList<String>) getValue(step, "init");
+            if (step.keySet().contains("chrome")) {
+                driver = new ChromeDriver();
+            }
+            if (step.keySet().contains("quit")) {
+                driver.quit();
+            }
+
+            if (step.keySet().contains("sleep")){
                 try {
-                    BasePage.getInstance().poInit(value.get(0), value.get(1));
-                } catch (Exception e) {
+                    Thread.sleep(Long.valueOf(getValue(step, "sleep").toString()));
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            if (key.contains(".")){
-                String[] objectMethod = key.split("\\.");
-                String object = objectMethod[0];
-                String method = objectMethod[1];
-                //解决了找方法的难题
-                BasePage.getInstance().getPO(object).stepRun(method);
+            if (step.keySet().contains("implicitly_wait")) {
+                driver.manage().timeouts().implicitlyWait(
+                        (int) getValue(step, "implicitly_wait"), TimeUnit.SECONDS);
+            }
+            if (step.keySet().contains("get")) {
+                driver.get(getValue(step, "get").toString());
+            }
+            if (step.keySet().contains("find")) {
+                ArrayList<By> bys = new ArrayList<>();
+                ((HashMap<String, String>) getValue(step, "find")).entrySet().forEach(stringStringEntry -> {
+                    if (stringStringEntry.getKey().contains("id")) {
+                        bys.add(By.id(stringStringEntry.getValue()));
+                    }
+
+                    if (stringStringEntry.getKey().contains("xpath")) {
+                        bys.add(By.xpath(stringStringEntry.getValue()));
+                    }
+
+                });
+                currentElement = driver.findElement(bys.get(0));
+            }
+
+            if (step.keySet().contains("click")) {
+                currentElement.click();
+            }
+            if (step.keySet().contains("send_keys")) {
+                //todo: 参数化
+                currentElement.sendKeys(getValue(step, "send_keys").toString());
             }
         });
     }
